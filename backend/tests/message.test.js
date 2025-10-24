4ea5156c39836d1ff9bcd00386807fd897771cb1
Shreya2776 <saxenashreya315@gmail.com>
2025-10-24 23:08:39 +0530

    yes

--- backend/tests/message.test.js
+++
@@ -1,9 +0,0 @@
-import request from "supertest";
-import app from "../src/app";
-
-describe("POST /api/messages/send", () => {
-  it("should return 400 for missing fields", async () => {
-    const res = await request(app).post("/api/messages/send").send({});
-    expect(res.statusCode).toEqual(400);
-  });
-});
